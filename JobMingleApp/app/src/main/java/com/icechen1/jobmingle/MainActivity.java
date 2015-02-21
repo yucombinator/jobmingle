7bf5763ba2d52bf02a37be82b65cc15e1b038aa9
Yu Chen Hou <houyuchen66@gmail.com>
2015-02-21 15:12:43 -0500

    sad

--- JobMingleApp/app/src/main/java/com/icechen1/jobmingle/MainActivity.java
+++ JobMingleApp/app/src/main/java/com/icechen1/jobmingle/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,6 +18,11 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity
@@ -135,6 +141,64 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            //add the view via xml or programmatically
+            SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) rootView.findViewById(R.id.frame);
+
+            final ArrayList al = new ArrayList<String>();
+            al.add("php");
+            al.add("c");
+            al.add("python");
+            al.add("java");
+
+            //choose your favorite adapter
+            final ArrayAdapter arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.item, R.id.textView, al );
+
+            //set the listener and the adapter
+            flingContainer.setAdapter(arrayAdapter);
+            flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+                @Override
+                public void removeFirstObjectInAdapter() {
+                    // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                    Log.d("LIST", "removed object!");
+                    al.remove(0);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onLeftCardExit(Object dataObject) {
+                    //Do something on the left!
+                    //You also have access to the original object.
+                    //If you want to use it just cast it (String) dataObject
+                    Toast.makeText(getActivity(), "Left!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onRightCardExit(Object dataObject) {
+                    Toast.makeText(getActivity(), "Right!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                    // Ask for more data here
+                    al.add("XML ".concat(String.valueOf(itemsInAdapter)));
+                    arrayAdapter.notifyDataSetChanged();
+                    Log.d("LIST", "notified");
+                    itemsInAdapter++;
+                }
+
+                @Override
+                public void onScroll(float v) {
+
+                }
+            });
+
+            // Optionally add an OnItemClickListener
+            flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClicked(int itemPosition, Object dataObject) {
+                    Toast.makeText(getActivity(), "Clicked!", Toast.LENGTH_SHORT).show();
+                }
+            });
             return rootView;
         }
 
